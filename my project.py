584114a59a45a403531494621ded088482a33030
hess-01 <132266200+hess-01@users.noreply.github.com>
2023-05-03 08:30:22 +0300

    Add files via upload

---
+++ my project.py
@@ -0,0 +1,11 @@
+
+
+#variable that has a value of your favourite football team.
+my fav_team is ="Alhilal"
+#variable “x“ has an integer value and a variable “y” has a float value
+x = 3
+y = 6.2
+ #Assign One Value to Multiple Variables (choose random names & values).
+ var1 = var2 = var 3 = "Hey"
+#Assign multiple Values to Multiple Variables (choose random names & values).
+name , age = " Hessah " , 26
